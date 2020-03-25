090e33fea211e1a07859927a553547cfaeff9bc1
UlisesGomez <ulisesgomez08live@gmail.com>
2020-03-25 15:51:14 -0600

    Test UI

---
+++ Script/Likes.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+function Resultado(operador){
+	 
+switch(operador){
+	case '+':
+		document.getElementById("operador").src="Icons/liken.png"; 	 
+	break;
+	case 'a':
+		document.getElementById("opera").src="Icons/liken.png"; 	 
+	break;
+	case 'b':
+		document.getElementById("uno").src="Icons/liken.png"; 	 
+	break;
+	case 'c':
+		document.getElementById("dos").src="Icons/liken.png"; 	 
+	break;
+	case 'd':
+		document.getElementById("tres").src="Icons/liken.png"; 	 
+	break;
+	case 'e':
+		document.getElementById("cuatro").src="Icons/liken.png"; 	 
+	break;
+	 		
+}
+ 
+} 
---
+++ Script/Publica.js
@@ -0,0 +1,11 @@
+function Resultado(operador){
+	var pub;
+	pub=document.getElementById("pub").value;
+	
+switch(operador){
+	case '+':
+		M=pub;
+	break;
+}
+document.getElementById("p").value=M;
+} 
